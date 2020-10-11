a386a0c66fdacb6c99df5774849cbc59f45a2b5e
Alizarinstone <Alizarinstone@gmail.com>
2020-10-11 14:21:25 +0300

    Update settings for deploy

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,9 @@
-let DEBUG = false;
-let HOST_URL = "https://chatup-vezaks.herokuapp.com";
-let SOCKET_URL = "wss://chatup-vezaks.herokuapp.com";
+let DEBUG = true;
+let HOST_URL = "http://127.0.0.1:8000";
+let SOCKET_URL = "ws://127.0.0.1:8000";
 if (DEBUG) {
-  HOST_URL = "http://127.0.0.1:8000";
-  SOCKET_URL = "ws://127.0.0.1:8000";
+  HOST_URL = "https://chatup-vezaks.herokuapp.com";
+  SOCKET_URL = "wss://chatup-vezaks.herokuapp.com";
 }
 
 export { HOST_URL, SOCKET_URL };
